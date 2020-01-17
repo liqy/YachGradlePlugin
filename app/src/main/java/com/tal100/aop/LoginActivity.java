b3ec88cd31e30126bc6ac128eab83d25fdc44535
liqingyi@100tal.com <Oxyful13>
2020-01-17 10:54:39 +0800

    登录功能实现

---
+++ app/src/main/java/com/tal100/aop/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.tal100.aop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * 登录页面
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
